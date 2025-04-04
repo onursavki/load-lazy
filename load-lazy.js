c92a00e98869b1d0d946b46e68cf363e4299d73a
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-04-04 21:40:38 +0300

    load-lazy.js

--- load-lazy.js
+++ load-lazy.js
@@ -1,12 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const images = document.querySelectorAll("img");
+
   images.forEach((img) => {
-    // 'loading' özelliği olmayan resimler için lazy loading ekleyelim
+    // Eğer 'loading' özelliği yoksa, lazy loading ekleyelim
     if (!img.hasAttribute("loading")) {
-      img.setAttribute("loading", "lazy");
-    }
-    // Eğer srcset varsa, lazy loading özelliğini de buraya ekleyelim (mobil için özellikle önemlidir)
-    else if (!img.hasAttribute("srcset")) {
       img.setAttribute("loading", "lazy");
     }
   });
